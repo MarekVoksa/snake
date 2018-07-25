6f20a7c5e5ce24c47bca2fb0f2496e2946e3313b
Marek Voksa <vest0079@gmail.com>
2018-07-25 22:30:56 +0200

    Created main.js and plan layout

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+/* Initial variables */
+
+let v = {
+  w: 40,
+  h: 40,
+  tick: 100
+};
+
+/* Cell prototype, snake object */
+
+/* Generate game board */
+
+/* Generate snake */
+
+/* onKeyDown function */
+
+/* Move function */
+
+/* onFoodPickup function */
+
+/* Init game */
